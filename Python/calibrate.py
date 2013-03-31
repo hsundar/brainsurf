a4c0988a9b85c09103bf1d23cd275bf3b0e732d7
Michael <wtrdrnkr@wtrdrnkr.(none)>
2013-03-31 17:00:10 -0500

    started calibrate() function

--- Python/calibrate.py
+++ Python/calibrate.py
@@ -5,7 +5,10 @@ import cv, sys, time
 #n_col = int( sys.argv[] )
     
 # Temp. variables
+n_img = 1
 src_img = '/home/wtrdrnkr/Documents/brainsurf/Data/calibration_target.jpg' # Input image of chessboard
+dims = (850, 1100)
+n_pts = dims[0]*dims[1] #=== number of points?
 dst_img = '/home/wtrdrnkr/Documents/brainsurf/Data/drawCorners.jpg' # Location to render image of found corners
 n_row = 8
 n_col = 6
@@ -14,7 +17,8 @@ patternSize = ( n_row, n_col )
 def main():
     '''Determines intrinsic and extrinsic parameters of camera based on images \
     passed to calibrate.py'''
-    corners()
+    c = corners()
+    calibrate(c)
     
 def corners():
     '''Finds corners and renders them in dst_img'''
@@ -33,13 +37,27 @@ def corners():
 
     # Render corners in matrix
     sys.stdout.write('Rendering found corners......')
-    img_out = cv.CreateMat(1100, 850, 0)
+    img_out = cv.CreateMat(dims[1], dims[0], 0)
     cv.DrawChessboardCorners( img_out, patternSize, corners, retval ); print('DONE')
+    
+    # Show preview image of rendered corners
+    cv.ShowImage('Out', img_out)
+    cv.WaitKey(2500)
     
     # Save rendered corners
     sys.stdout.write('Saving file......')
     cv.SaveImage(dst_img, img_out); print('DONE'); print('    Location: %s')%dst_img
+    return corners
 
+def calibrate(corners):
+    # === NOT WORKING YET
+    opts = cv.CreateMat(n_img * n_pts, 3, cv.CV_32FC1)
+    ipts = cv.CreateMat(corners* n_pts, 2, cv.CV_32FC1)
+    npts = cv.CreateMat(n_img, 1, cv.CV_32SC1)
+    
+    
+    
+    
 main()
 
 
