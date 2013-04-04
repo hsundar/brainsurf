3b54091670aae4fce62c321fdf59b5d2a88437bf
Michael <wtrdrnkr@wtrdrnkr.(none)>
2013-04-03 23:04:58 -0500

    updated comments

--- Python/calibrate.py
+++ Python/calibrate.py
@@ -2,44 +2,42 @@
 #
 # Usage: python calibrate.py n_row n_col path_to_imgs
 #
-# Last modified: 4/1/2013
+# Last modified: 4/3/2013
 #
 # Currently working on:
+#    - convert to 8bit
 #    - calibrate() function
 #    - Auto recognize dimensions of input imgs
 #    - Auto recognize patternSize
 
 import cv, sys, os
-# Global Variables
-if len(sys.argv) > 1:
-    # If from command line
+if len(sys.argv) > 1: #if called from command line
     n_row = int( sys.argv[1] )
     n_col = int( sys.argv[2] )
     path_to_imgs = str( sys.argv[3] )
-
-else:
-    # Temp. variables (for development)
+else: # Temp. variables (for development)
     path_to_imgs = '/home/wtrdrnkr/Pictures/Calib_images/' # Retrieves from and saves to
     n_row = 9
     n_col = 6
 
+# Global Variables
 dims = (3744, 2104) # Dimensions of input img
 n_pts = dims[0]*dims[1] #=== number of points=pixels?
 patternSize = ( n_row, n_col )
 
 def main():
-    '''Determines intrinsic and extrinsic parameters of camera based on images \
-    passed to calibrate.py'''
+    '''Determines intrinsic and extrinsic parameters of camera by use of images \
+    in the path passed to calibrate.py'''
     cornerList = []
     # Add corners from each img into ptList
     # Each set of corners is its own element
     for src_img in os.listdir(path_to_imgs):
         if 'crn' not in src_img: # prevents corner()ing of rendered corners
-            cornerList.append(corners(src_img))
+            cornerList.append(getcorners(src_img))
     print(cornerList)
     #calibrate(c)
     
-def corners(src_img):
+def getcorners(src_img):
     '''Finds corners and renders them in dst_img'''
     # Load/convert image
     print('Loading %s as grayscale......')%src_img,
@@ -49,7 +47,7 @@ def corners(src_img):
     # Find corners
     print('Finding corners......'),
     retval, corners = cv.FindChessboardCorners( img_in, patternSize )
-    if retval != 1:print('CORNERS NOT FOUND');print('END');quit()
+    if retval != 1:print('CORNERS NOT FOUND'); print('END'); quit()
     else:
         # Refine corners: !!!!check parameters!!!! ===
         corners = cv.FindCornerSubPix(img_in, corners, (5,5), (-1,-1), (cv.CV_TERMCRIT_EPS,0,.01)); print('DONE')
