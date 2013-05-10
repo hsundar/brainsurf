46c11c7d074c7fd161f20e08566066e481ca426a
Michael Musslewhite <mdmusslewhite@gmail.com>
2013-05-10 16:24:56 -0500

    mroe testing

--- Python/evalFeats.py
+++ Python/evalFeats.py
@@ -1,36 +1,48 @@
 import cv, sys, os
 # Problems:
-# == Cant find feature detection algorithms (python)
+# == Python feature detection algorithms
+# cv.ExtractSURF()
+# cv.GetStarKeypoints()
+# cv.Canny(image, edges, threshold1, threshold2)
+# cv.CornerEigenValsAndVecs(image, eigenvv, blockSize)
+# cv.CornerHarris(image, harris_dst, blockSize)
+# cv.FindCornerSubPix(image, corners, win, zero_zone, criteria)
+# cv.GoodFeaturesToTrack(image, eigImage, tempImage, cornerCount, qualityLevel, minDistance)
+# cv.HoughLines2(image, storage, method, rho, theta, threshold)
+# cv.PreCornerDetect(image, corners)
+
 # == How to get rid of previous detected pts as trackbar moves
-
-image = cv.LoadImageM('/home/michaelm/Documents/ecog/grids.jpg')
+img = '/home/michaelm/Documents/ecog/grids.jpg'
+image = cv.LoadImageM(img)
 cur_pos = 0
-def main(image):
-    name = 'evalFeats'
-    # Point attributes
-    radius = 30
-    thickness = 2
-    connectivity = 8
-    green = cv.CV_RGB(0,250,0)
-    
-    # Create window & trackbar
-    cv.NamedWindow(name, 1)
-    cv.CreateTrackbar('Threshold', name, cur_pos, 255, switch_callback)
 
-    while True:
-        # Detected points
-        testpt = (cv.GetTrackbarPos('Threshold', name),cv.GetTrackbarPos('Threshold', name))
-        pts = [testpt] #list of points returned from feature detection
-        for pt in pts:
-            cv.Circle(image,pt,radius,green,thickness,connectivity)
-        
-        cv.ShowImage(name, image)
-
-        # Key to quit
-        if cv.WaitKey( 15 ) == 27: # ESC
-            break
+def main():
+    cv.NamedWindow('Window', )
     
-    cv.DestroyWindow( name )
+    
+main()
+# def main(image):
+#     name = 'evalFeats'
+#     # Point attributes
+#     radius = 30
+#     thickness = 2
+#     connectivity = 8
+#     green = cv.CV_RGB(0,250,0)
+#     # Create window & trackbar
+#     cv.NamedWindow(name, 1)
+#     cv.CreateTrackbar('Threshold', name, cur_pos, 255, switch_callback)
+#     # Show pts
+#     while True:
+#         # Detected points
+#         testpt = (cv.GetTrackbarPos('Threshold', name),cv.GetTrackbarPos('Threshold', name))
+#         pts = [testpt] #list of points returned from feature detection
+#         for pt in pts:
+#             cv.Circle(image,pt,radius,green,thickness,connectivity)
+#         cv.ShowImage(name, image)
+#         # Key to quit
+#         if cv.WaitKey( 15 ) == 27: # ESC
+#             break
+#     cv.DestroyWindow( name )
 
 def switch_callback( position ):
     cur_pos = position
@@ -47,12 +59,11 @@ def test(image):
     connectivity = 8
     green = cv.CV_RGB(0,250,0)
     
-    pts = []
+    
     alg = cv.GoodFeaturesToTrack(img, eig_image, temp_image, cur_pos, 0.04, 1.0, useHarris=True)
-    for (x,y) in alg:
-        pts.append((x,y))
+    pts = [(x,y) for (x,y) in alg]
     cv.NamedWindow('test')
-    cv.CreateTrackbar('Threshold', name, cur_pos, 255, switch_callback)
+    #cv.CreateTrackbar('Threshold', name, cur_pos, 255, switch_callback)  
     
     while True:
         for pt in pts:
@@ -61,25 +72,4 @@ def test(image):
         if cv.WaitKey(15) == 27:
             break
     cv.DestroyWindow('test')    
-test(image)
-
-# def switch_callback(pos):
-#     position = pos
-#     if position <= 255 and position >=0:
-#         return 'hi'
-#     else:
-#         return 'hi2'
-#         
-# def main():
-#     imgpath = '/home/wtrdrnkr/Documents/ecog/grids.jpg'
-#     img = cv.LoadImageM(imgpath)
-#     cv.NamedWindow('Test', flags=cv.CV_WINDOW_AUTOSIZE)
-#     cv.CreateTrackbar('Threshold', 'Test', 50, 255, )
-#     cv.ShowImage('Test', img)
-#     cv.WaitKey(0)
-#     print('DONE')
-#     
-#     # cv.SetTrackbarPos('trackbarName', 'windowName', int(pos)) #?
-#     # cv.WaitKey()
-#     
-# main()
+#test(image)
