4d96e5d1afc6fb16a7487e0ecf8a54a42a605792
Michael <wtrdrnkr@wtrdrnkr.(none)>
2013-05-11 18:54:42 -0500

    Can't figure out how to refresh image with new pts

--- Python/evalFeats.py
+++ Python/evalFeats.py
@@ -1,26 +1,55 @@
-import cv, sys, os
-# Problems:
-# == Python feature detection algorithms
+import cv
 # cv.ExtractSURF()
 # cv.GetStarKeypoints()
 # cv.Canny(image, edges, threshold1, threshold2)
-# cv.CornerEigenValsAndVecs(image, eigenvv, blockSize)
 # cv.CornerHarris(image, harris_dst, blockSize)
+# cv.CornerMinEigenVal(image, eigenval, blockSize)
+# cv.CornerEigenValsAndVecs(image, eigenvv, blockSize)
 # cv.FindCornerSubPix(image, corners, win, zero_zone, criteria)
 # cv.GoodFeaturesToTrack(image, eigImage, tempImage, cornerCount, qualityLevel, minDistance)
 # cv.HoughLines2(image, storage, method, rho, theta, threshold)
 # cv.PreCornerDetect(image, corners)
 
-# == How to get rid of previous detected pts as trackbar moves
-img = '/home/michaelm/Documents/ecog/grids.jpg'
+# WaitKey == 27 not working properly
+# Previous pts not going away
+
+
+
+img = '/home/wtrdrnkr/Documents/ecog/grids.jpg'
 image = cv.LoadImageM(img)
-cur_pos = 0
+windowName = 'evalFeats.py'
+trackbarName = 'Adjust'
+strtPos = 0
+endPos = 255
+
+radius = 30
+thickness = 2
+connectivity = 8
+green = cv.CV_RGB(0,250,0)
+
 
 def main():
-    cv.NamedWindow('Window', )
+    cv.NamedWindow(windowName, cv.CV_WINDOW_AUTOSIZE)
+    cv.CreateTrackbar(trackbarName, windowName, strtPos, endPos, barPos)
+    while True:
+        showimg()
+
+def showimg():
+    
+    while cv.WaitKey(10) != 27: # ESC
+        cv.ShowImage(windowName, image)
+        pts = [(x,y) for (x,y) in [(cv.GetTrackbarPos(trackbarName, windowName),cv.GetTrackbarPos(trackbarName, windowName))]]
+        for pt in pts:
+            cv.Circle(image,pt,radius,green,thickness,connectivity)  
+                   
+def barPos(x):
+    strtPos = x
+
     
     
-main()
+main()    
+
+
 # def main(image):
 #     name = 'evalFeats'
 #     # Point attributes
