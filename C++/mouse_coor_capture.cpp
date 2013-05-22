b13380a75548650553d229b70aec423305cf37f0
VenkatKS <Venkat.S@utexas.edu>
2013-05-22 18:18:47 -0500

    Update mouse_coor_capture.cpp
    Resizes huge images to 1000 by 500.
    Changed coordinate storage system for huge images (larger than the screen)
    Changed the color of re-populated coordinates to light grey while keeping the color of newly added coordinates deep blue.

--- C++/mouse_coor_capture.cpp
+++ C++/mouse_coor_capture.cpp
@@ -1,6 +1,7 @@
 //#include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include "wtypes.h"
 #include "opencv\cv.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/core_c.h"
@@ -18,53 +19,137 @@ ifstream infile;
 IplImage* img;
 int posx;
 int posy;
+int saveit;
+int saveity;
+boolean isit;
+IplImage *myresize;
 
 void mouseEvent(int evt, int x, int y, int flags, void* param){
+	int virx=0;
+	int viry=0;
     if(evt==CV_EVENT_LBUTTONDOWN){
         //printf("%d %d\n",x,y);
+		//pos/oldsize = x/newsize
+		if(isit){
+		virx=(saveit*x)/(myresize->width);
+		viry=(saveity*y)/(myresize->height);
+		//printf("\n%d %d\n",virx,viry);
+		//printf("Actual: %d %d",x,y);
+		}
 		stringstream ss;
+		if(isit){
+			ss<<virx;
+		}
+		else{
 		ss << x;
+		}
 		string strx = ss.str();
 
 		string myStr="";
 		myStr+=strx;
 
 		stringstream ssy;
+		if(isit)
+		{
+			ssy<<viry;
+		}
+		else{
 		ssy<<y;
+		}
 		string stry = ssy.str();
 		myStr+=" ";
 		myStr+=stry;
 		myStr+="\n";
 		myFile<<myStr;
+
+		if(isit)
+		{
+			cvCircle(myresize,cvPoint(x,y),2,CV_RGB(0,0,255),-1);
+			cvShowImage("Mouse Coordinate Calc", myresize);
+		}
+		else{
 		cvCircle(img,cvPoint(x,y),2,CV_RGB(0,0,255),-1);
 		
 		cvShowImage("Mouse Coordinate Calc", img);
-
+		}
 	}
+}
+
+void GetDesk(int& horizontal, int& vertical)
+{
+   RECT desktop;
+   // Get a handle to the desktop window
+   const HWND hDesktop = GetDesktopWindow();
+   // Get the size of screen to the variable desktop
+   GetWindowRect(hDesktop, &desktop);
+   // The top left corner will have coordinates (0,0)
+   // and the bottom right corner will have coordinates
+   // (horizontal, vertical)
+   horizontal = desktop.right;
+   vertical = desktop.bottom;
 }
 
 
 int main(int argc, char** argv)
 {
+		isit=false;
+
 		int popx=0;
 		int popy=0;
+		int size=0;
+		boolean newfile=false;
 		string mystring="";
 		
 		
 		
-        cvNamedWindow("Mouse Coordinate Calc");
+        
 
 		img = cvLoadImage(argv[1]);
-        cvShowImage("Mouse Coordinate Calc", img);
+		saveit=img->width;
+		saveity=img->height;
+		//image resizing
+		GetDesk(popx,popy);
+		
+		if((img->width)>popx)
+		{
+			printf("Image too big for monitor...resizing\n");
+			//img->width=1000;
+			isit=true;
+
+		}
+		if((img->height)>popy)
+		{
+			printf("Image too big for monitor...resizing\n");
+			//img->height=1000;
+			isit=true;
+		}
+		if(isit)
+		{
+			myresize = cvCreateImage(cvSize(1000,500),img->depth,img->nChannels);
+			cvResize(img,myresize);
+			cvShowImage("Mouse Coordinate Calc", myresize);
+
+
+		}
+		cvNamedWindow("Mouse Coordinate Calc",CV_WINDOW_AUTOSIZE);
+		if(isit){
+        cvShowImage("Mouse Coordinate Calc", myresize);
+		}
+		else
+		{
+			cvShowImage("Mouse Coordinate Calc", img);
+		}
 		infile.open(argv[2]);
 		if(!infile)
 		{
 
 			printf("%s","Cannot find file, assuming unpopulated list.");
+			newfile=true;
 		}
 		else
 		{
 			printf("File found. Re-populating image...");
+			newfile=false;
 			 while(!infile.eof())
 			{
 			getline(infile,mystring,' ');
@@ -77,14 +162,22 @@ int main(int argc, char** argv)
 			posy=atoi(mystring.c_str());
 
 			//printf("%s","\n");
-			
-			cvCircle(img,cvPoint(posx,posy),2,CV_RGB(0,0,255),-1);
-		
-			cvShowImage("Mouse Coordinate Calc", img);
+			if(isit)
+			{
+				cvCircle(myresize,cvPoint(((posx*myresize->width)/saveit),(posy*myresize->height)/saveity),2,CV_RGB(0,255,255),-1);
+				cvShowImage("Mouse Coordinate Calc", myresize);
+
+			}
+			else{
+				cvCircle(img,cvPoint(posx,posy),2,CV_RGB(0,0,255),-1);
+				cvShowImage("Mouse Coordinate Calc", img);
+			}
+				
 			}
 
 		}
 		 myFile.open(argv[2],std::ios_base::app);
+		
         cvSetMouseCallback("Mouse Coordinate Calc", mouseEvent, 0);
         
         
@@ -95,5 +188,6 @@ int main(int argc, char** argv)
         cvDestroyWindow("Mouse Coordinate Calc");
         cvReleaseImage(&img);
 		myFile.close();
+		
         return 0;
 } 
